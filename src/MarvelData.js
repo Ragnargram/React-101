c87d39fb00b901d64e6165f42d06bd486ff2248f
Ragnargram <150048388+Ragnargram@users.noreply.github.com>
2024-01-15 06:02:26 -0800

    Add files via upload
    tnx

---
+++ src/MarvelData.js
@@ -0,0 +1,97 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "EREN YEAGER",
+    alias: "Advancing Titan",
+    img1: "https://i1.sndcdn.com/artworks-ionT6wTuKAEzJFhh-VQJufw-t500x500.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Titan Transformative",
+      "Regenerate",
+      "Shifter",
+    ],
+    team: "The Rogue squad",
+    description:
+      "Eren Yeager is the main protagonist in Attack on Titan. Driven by a fervent desire for freedom and fueled by the power of the Titans, he evolves from a determined youth to a complex character with immense responsibilities and consequences.",
+  },
+  {
+    name: "Monkey D. Luffy",
+    alias: "Straw Hat",
+    abilities: ["Devil fruit Awakening", "Future Sight", "Durable Body"],
+    team: "Pirates",
+    img1: "https://qph.cf2.quoracdn.net/main-qimg-ccca700b5e28038bd161668f9a2ecf77",
+    homeworld: "Earth",
+    description:
+      "Monkey D. Luffy, the spirited protagonist of One Piece, possesses a resilient and stretchable physique due to consuming the Gomu Gomu no Mi Devil Fruit. Driven by a charismatic personality and a dream of becoming the Pirate King, Luffy navigates a vibrant and perilous world in search of the elusive One Piece treasure.",
+  },
+  {
+    name: "Levi Ackerman",
+    alias: "Captain Ackerman",
+    abilities: ["Omnidirectional Expertise", "Unrivaled Agility."],
+    team: "The Rogue Squad",
+    img1: "https://www.cheatsheet.com/wp-content/uploads/2021/12/Levi-Ackerman-Attack-on-Titan.jpeg?w=1200&h=898&strip=all&quality=89",
+    homeworld: "Earth",
+
+    description:
+      "Levi Ackerman, humanity's strongest soldier, is renowned for his unparalleled agility and precision in combat, wielding the omnidirectional mobility gear with unmatched skill. His strategic brilliance and unwavering determination make him a formidable force against the Titan menace in Attack on Titan.",
+  },
+  {
+    name: "Baki Hanma",
+    alias: "The Champion",
+    abilities: ["Superspeed", "Stamina"],
+    team: "The Grappler",
+    img1: "https://i.pinimg.com/originals/f1/f7/92/f1f792b53731b6fe9acf84833783072b.jpg",
+    homeworld: "Earth",
+
+    description:
+      " An unparalleled martial artist driven by an insatiable desire for strength. His relentless pursuit of combat perfection leads him to face formidable opponents in intense and visceral battles.",
+  },
+
+  // Spider-Man
+  {
+    name: "Saitama",
+    alias: "The One Punch Man",
+    abilities: ["Dexterity", "Super Senses"],
+    team: "New Blizzard Group",
+    img1: "https://i.pinimg.com/736x/f7/39/83/f739831cdf5f360070a5cf135285b1ef.jpg",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      " An unbeatable hero who defeats any foe with a single punch, but his overwhelming strength has left him bored and searching for a challenge in life.",
+  },
+
+  {
+    name: "Goku",
+    alias: "Kakarot",
+    abilities: ["Teleportation", "Healing"],
+    team: "Dragon",
+    img1: "https://i.pinimg.com/236x/36/f6/0a/36f60af03a3aaea4e8b271edfc2e13db.jpg",
+    homeworld: "Planet Vegata",
+    description:
+      "Goku is a determined Saiyan warrior, endlessly pursuing strength and defending his world, known for his kindness and unwavering spirit in the face of any challenge.",
+  },
+
+  {
+    name: "Tetsuya Kuroko",
+    alias: "Kurokocchi",
+    abilities: ["Phantom Shot", "misdirection"],
+    team: "Seirin BB team",
+    img1: "https://i.pinimg.com/474x/f0/bb/df/f0bbdf6fa3c06082b56157e5e733e8a7.jpg",
+    homeworld: "Earth",
+    description:
+      " A skilled but unassuming player, utilizing his stealth and passing abilities as the Phantom Sixth Man to support his team's success on the court through clever strategies and teamwork.",
+  },
+
+  {
+    name: "Yoichi Isagi",
+    alias: "Egoist",
+    abilities: [" Transcendence Sight", "Metavision"],
+    team: "Team Z",
+    img1: "https://preview.redd.it/isagi-yoichi-artwork-by-me-v0-6fvg0k1s2yga1.jpg?width=640&crop=smart&auto=webp&s=c2d5da7dbdfe0f2fb72328c0a6828677c8b0d64a",
+    homeworld: "Earth",
+    description:
+      "Yoichi Isagi is a resilient soccer talent, driven by his ambition to excel and evolve, showcasing adaptability and determination on the field to achieve his goals in the world of football.",
+  },
+];
+
+console.log(characters);
